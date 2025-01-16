f88a07c68f3ab193950ac1a926eae1a3df2e3191
vimukthiwaththegama <vwaththegama@gmail.com>
2025-01-16 22:29:24 +0530

    16/01/2025

--- part-2/bookscraper/bookscraper/spiders/bookspider.py
+++ part-2/bookscraper/bookscraper/spiders/bookspider.py
@@ -1,10 +1,19 @@
 import scrapy
-
+from pathlib import Path
 
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
-    allowed_domains = ["books.toscrape.com"]
-    start_urls = ["https://books.toscrape.com"]
-
+    
+    def start_requests(self):
+     
+        start_urls = [  "https://books.toscrape.com",
+                        "https://quotes.toscrape.com/page/1/",
+                        "https://quotes.toscrape.com/page/2/"]
+        for url in start_urls:
+            yield scrapy.Request(url=url,callback=self.parse)
+            
     def parse(self, response):
-        pass
+        page = response.url.split("/")[-2]
+        filename = f"quotes-{page}.html"
+        Path(filename).write_bytes(response.body)
+        self.log(f"Saved file {filename}")
